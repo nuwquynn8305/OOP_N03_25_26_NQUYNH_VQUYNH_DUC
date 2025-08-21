4297803d0b63d43c0801ddf0cc16047506bf1622
Như Quỳnh <dinhq9392@gmail.com>
2025-08-21 13:00:51 +0000

    java

---
+++ src/bia.java
@@ -0,0 +1,17 @@
+public class Bia {
+
+    public String tenbia;
+    public String nhanbia;
+    public String mausac;
+    public int luuluong;
+
+    public void uong(String tenbia) {
+        System.out.println("Bia::" + tenbia);
+    }
+    public String getmausac(){
+        return mausac;
+    }
+    public String getten(){
+        return tenbia;
+    }
+}
