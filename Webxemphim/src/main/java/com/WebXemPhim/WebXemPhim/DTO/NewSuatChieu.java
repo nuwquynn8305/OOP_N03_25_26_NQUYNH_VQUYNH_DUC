cc7a700c75f85d1e2c8c666a6136ace3b48080b5
vannquynh <hoangvanquynh404@gmail.com>
2025-09-19 10:21:20 +0000

    edit

---
+++ Webxemphim/src/main/java/com/WebXemPhim/WebXemPhim/DTO/NewSuatChieu.java
@@ -0,0 +1,61 @@
+package com.WebXemPhim.WebXemPhim.DTO;
+
+import java.util.List;
+
+public class NewSuatChieu {
+    private int idPhim;
+    private int idNgayChieu;
+    private int idTinh;
+    private int idDiaDiem;
+    private int idGioChieu;
+    private int idLoaiRap;
+
+
+    public int getIdPhim() {
+        return idPhim;
+    }
+
+    public void setIdPhim(int idPhim) {
+        this.idPhim = idPhim;
+    }
+
+    public int getIdNgayChieu() {
+        return idNgayChieu;
+    }
+
+    public void setIdNgayChieu(int idNgayChieu) {
+        this.idNgayChieu = idNgayChieu;
+    }
+
+    public int getIdTinh() {
+        return idTinh;
+    }
+
+    public void setIdTinh(int idTinh) {
+        this.idTinh = idTinh;
+    }
+
+    public int getIdDiaDiem() {
+        return idDiaDiem;
+    }
+
+    public void setIdDiaDiem(int idDiaDiem) {
+        this.idDiaDiem = idDiaDiem;
+    }
+
+    public int getIdGioChieu() {
+        return idGioChieu;
+    }
+
+    public void setIdGioChieu(int idGioChieu) {
+        this.idGioChieu = idGioChieu;
+    }
+
+    public int getIdLoaiRap() {
+        return idLoaiRap;
+    }
+
+    public void setIdLoaiRap(int idLoaiRap) {
+        this.idLoaiRap = idLoaiRap;
+    }
+}
